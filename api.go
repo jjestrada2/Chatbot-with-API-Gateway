b61fbbd5aade4a68207dd30964d19155382b74cc
jjestrada3 <juan@neuroleapmail.com>
2024-03-17 23:28:49 -0700

    register project service

--- api.go
+++ api.go
@@ -23,13 +23,15 @@ func (s *APIServer) Serve() {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
 
-	//Registering our services
+	projectService := NewProjectService(s.store)
+	projectService.RegisterRoutes(subrouter)
+
+	userService := NewUserService(s.store)
+	userService.RegisterRoutes(subrouter)
+
 
 	tasksService := NewTasksService(s.store)
 	tasksService.RegisterRoutes(subrouter)
-
-	usersService := NewUserService(s.store)
-	usersService.RegisterRoutes(subrouter)
 
 	log.Println("Starting the API server at", s.addr)
 
