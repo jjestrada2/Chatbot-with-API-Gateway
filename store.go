55e4b2b00fa83c717e0c014274156fc5f5a03749
jjestrada3 <juan@neuroleapmail.com>
2024-03-17 19:10:07 -0700

    handleCreateTask structure implemented

--- store.go
+++ store.go
@@ -9,6 +9,9 @@ type Storage struct {
 type Store interface {
 	// Users
 	CreateUser() error
+
+	//Tasks
+	CreateTask(t *Task) (*Task, error)
 }
 
 func NewStore(db *sql.DB) *Storage {
@@ -19,4 +22,20 @@ func NewStore(db *sql.DB) *Storage {
 
 func (s *Storage) CreateUser() error {
 	return nil
+}
+
+func (s *Storage) CreateTask(t *Task) (*Task, error) {
+	rows, err := s.db.Exec("INSERT INTO tasks (name, status, project_id, assigned_to) VALUES (?, ?, ?, ?)", t.Name, t.Status, t.ProjectID, t.AssignedToID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := rows.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	t.ID = id
+	return t, nil
 }
--- tasks.go
+++ tasks.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+var errNameRequired = errors.New("name is required")
+var errProjectIDRequired = errors.New("project id is required")
+var errUserIDRequired = errors.New("user id is required")
 
 type TasksService struct {
 	store Store
@@ -20,7 +27,23 @@ func (s *TasksService) RegisterRoutes(r *mux.Router) {
 }
 
 func (s *TasksService) handleCreateTask(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
 
+	defer r.Body.Close()
+	var task *Task
+	err = json.Unmarshal(body, &task)
+
+	if err != nil {
+		return
+	}
+
+	t, err := s.store.CreateTask(task)
+	if err != nil {
+		return
+	}
 }
 
 func (s *TasksService) handleGetTask(w http.ResponseWriter, r *http.Request) {
