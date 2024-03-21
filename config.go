6d162c837629f759e795d9d29fe5ae7e3be79bd1
jjestrada3 <juan@neuroleapmail.com>
2024-03-21 14:52:27 -0700

    fix env variable problem

--- config.go
+++ config.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -17,12 +20,16 @@ type Config struct {
 var Envs = initConfig()
 
 func initConfig() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	return Config{
 		Port:       getEnv("PORT", "8080"),
-		DBUser:     getEnv("DB_USER", "root"),
-		DBPassword: getEnv("DB_PASSWORD", "Alejo98519527#"),
-		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
-		DBName:     getEnv("DB_NAME", "projectmanager"),
+		DBUser:     getEnv("DB_USER", "admin"),
+		DBPassword: getEnv("DB_PASSWORD", "password"),
+		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "fastlydb.czcm6osyi4ii.us-east-1.rds.amazonaws.com"), getEnv("DB_PORT", "3306")),
+		DBName:     getEnv("DB_NAME", "fastlydb"),
 		JWTSecret:  getEnv("JWT_SECRET", "randomjwtsecretkey"),
 	}
 }
